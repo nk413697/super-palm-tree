d3562bd7dc5bbb41e535780b8fa5d8647ccd712a
nk413697 <nrmlkmr12@gmail.com>
2026-08-08 13:27:11 +0530

    Add alert for member.js loading

--- member.js
+++ member.js
@@ -1,3 +1,4 @@
+alert("member.js loaded");
 const scriptURL =
 "https://script.google.com/macros/s/AKfycbzrVaqJF0tOmx3Z3B5Nd1f52-g6h1OinEhM2joHdKYhTN4NxHp9t8ptdHqFGxNh3yf8_g/exec";
 const form = document.getElementById("memberForm");
